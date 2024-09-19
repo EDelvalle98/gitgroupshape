4d6186c91b7f43fdff712a9560fb49825328b1c9
EDelvalle98 <efrain.delvalle@hudsoncatholic.org>
2024-09-19 12:02:29 -0400

    first commit

---
+++ src/main/java/Main/shape.java
@@ -0,0 +1,17 @@
+package Main;
+
+public class shape {
+    public String color;
+    public double area;
+    public double perimeter;
+
+
+    public void calculateArea() {
+        System.out.println("This method calculates Area");
+    }
+
+    public void calculatePerimeter() {
+        System.out.println("This method calculates Perimeter");
+
+    }
+}
